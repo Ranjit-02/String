b6a798e142ee001db061bfce947064687cc08990
Ranjith-02 <119964117+Ranjit-02@users.noreply.github.com>
2022-12-08 23:30:47 +0530

    Add files via upload

---
+++ character.java
@@ -0,0 +1,18 @@
+public class character{
+    public static void main(String args[]){
+        String s="India is my country";
+        char c[]=new char[s.length()];
+         for(int i=0; i<c.length;i++){
+             c[i]=s.charAt(i);
+        }
+    int counter=0;
+    for(int i=0;i<c.length;i++){
+        if(c[i]=='a'){
+        counter=counter+1;
+        }
+    }
+    System.out.println(counter);
+    }
+}
+
+
